60885afcbcaf5c6bce34c1011aa7c9986b8fe452
Pawan <paletipawankumar@gmail.com>
2023-02-23 22:56:52 +0530

    Adding bubble sort algoritm

--- Basics/Arrays/Traversing.c
+++ Basics/Arrays/Traversing.c
@@ -26,9 +26,26 @@ void linearSearch(int arr[], int size, int target)
     }
 }
 
+// bubble sort algoritm
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 void main()
 {
-    int n, target;
+    int n, target, ch;
     printf("Enter the number of element you want to enter in the array \n");
     scanf("%d", &n);
 
@@ -39,14 +56,31 @@ void main()
         printf("Enter the element at index %d ", i);
         scanf("%d", &arr[i]);
     }
-    // printint the array
-    printf("The details of the array are :\n");
-    traversal(arr, n);
 
-    // linear search algorithm
-    printf("Enter the element you are looking for in the array \n");
-    scanf("%d", &target);
-    linearSearch(arr, n, target);
+    printf("Enter your choice \n");
+    scanf("%d", &ch);
+
+    switch (ch)
+    {
+    case 1:
+        printf("The details of the array are :\n");
+        // printint the array
+        traversal(arr, n);
+        break;
+
+    case 2:
+        // linear search algorithm
+        printf("Enter the element you are looking for in the array \n");
+        linearSearch(arr, n, target);
+        scanf("%d", &target);
+        break;
+
+    case 3:
+        // bubble sort algorith
+        bubbleSort(arr, n);
+        printf("After sorting \n");
+        traversal(arr, n);
+    }
 }
 // int arr[] = {1, 2, 3, 4, 5, 6};
 // int size = sizeof(arr) / sizeof(int);
