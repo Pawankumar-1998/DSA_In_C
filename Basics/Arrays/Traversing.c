0b190f5529cfdb8d521dc3fcf9ae8c35f99139e5
Pawan <paletipawankumar@gmail.com>
2023-02-23 21:58:06 +0530

    Adding code of linear search algorithm

--- Basics/Arrays/Traversing.c
+++ Basics/Arrays/Traversing.c
@@ -8,9 +8,27 @@ void traversal(int arr[], int n)
         printf(" the element at index %d is %d \n", i, arr[i]);
     }
 }
+
+// linear search algorithm
+void linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            printf("the element is located at %d index of the array \n", i);
+            break;
+        }
+        if (i == size)
+        {
+            printf("The element you are looking for is not present in the array \n");
+        }
+    }
+}
+
 void main()
 {
-    int n;
+    int n, target;
     printf("Enter the number of element you want to enter in the array \n");
     scanf("%d", &n);
 
@@ -24,4 +42,12 @@ void main()
     // printint the array
     printf("The details of the array are :\n");
     traversal(arr, n);
+
+    // linear search algorithm
+    printf("Enter the element you are looking for in the array \n");
+    scanf("%d", &target);
+    linearSearch(arr, n, target);
 }
+// int arr[] = {1, 2, 3, 4, 5, 6};
+// int size = sizeof(arr) / sizeof(int);
+// printf("the size is %d \n ", size);
