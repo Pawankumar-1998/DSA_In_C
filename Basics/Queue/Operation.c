2cf35c572ca6486946471562798a331ceb97792c
Pawan <paletipawankumar@gmail.com>
2023-03-02 00:48:10 +0530

    Adding code for all the operation on the queue

--- Basics/Queue/Operation.c
+++ Basics/Queue/Operation.c
@@ -19,7 +19,7 @@ void main()
         printf("2.Deletion \n");
         printf("3.Traversal \n");
         printf("4. Exit  \n");
-        printf("Enter your choice");
+        printf("Enter your choice \n");
         scanf("%d", &ch);
         switch (ch)
         {
@@ -27,7 +27,6 @@ void main()
             printf("Enter the element you want to enter \n");
             scanf("%d", &element);
             insert(element);
-            printf("Succesfully entered %d into the queue \n", element);
             break;
 
         case 2:
@@ -40,6 +39,7 @@ void main()
 
         case 4:
             exit(0);
+            break;
 
         default:
             printf("Default value entered \n");
@@ -58,16 +58,38 @@ void insert(int element)
     {
         queue[rear] = element;
         rear++;
+        printf("Succesfully entered %d into the queue \n", element);
     }
 }
 
 // function for deleting a item from the queue
 void delete()
 {
-    
+    if (front == rear)
+    {
+        printf("there are no elements to print \n");
+    }
+    else
+    {
+        printf("Element %d is being deleted ", queue[front]);
+        for (int i = 0; i < rear - 1; i++)
+        {
+            queue[i] = queue[i + 1];
+        }
+        rear--;
+    }
 }
 
 // function for traversing through the queue
 void traversal()
 {
+    if (front == rear)
+    {
+        printf("No elements in the Queue to display \n");
+    }
+    else
+    {
+        for (int i = front; i < rear; i++)
+            printf("%d \n", queue[i]);
+    }
 }
